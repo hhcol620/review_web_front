8dda2b791b5bb65e6eeb9282fbeee4be980473cf
huanghe <h13299630387@163.com>
2025-06-16 22:34:27 +0800

    滴滴国际外卖

--- js_code_polyfill/instanceof.js
+++ js_code_polyfill/instanceof.js
@@ -1,14 +1,14 @@
 // instanceof 运算符用于检测构造函数的prototype属性是否出现在某个实例对象的原型链上
 
 const myInstanceof = (left, right) => {
-    // 基本数据都返回false
-    if (typeof left !== 'object' || left === null) return false;
-    let proto = Object.getPrototypeOf(left); //  获取left的原型  判断是否和right相等  如果不等继续向上查找 如果为null 的话则返回false
-    while (true) {
-        if (proto === null) return false;
-        if (proto === right.prototype) return true;
-        proto = Object.getPrototypeOf(proto); // 沿着原型链继续向上寻找
-    }
+  // 基本数据都返回false
+  if (typeof left !== 'object' || left === null) return false;
+  let proto = Object.getPrototypeOf(left); //  获取left的原型  判断是否和right相等  如果不等继续向上查找 如果为null 的话则返回false
+  while (true) {
+    if (proto === null) return false;
+    if (proto === right.prototype) return true;
+    proto = Object.getPrototypeOf(proto); // 沿着原型链继续向上寻找
+  }
 };
 
 // const myInstanceof = (left, right) => {
@@ -22,6 +22,19 @@ const myInstanceof = (left, right) => {
 //         proto = Object.getPrototypeOf(proto); //  沿着原型链继续向上寻找
 //     }
 // };
+
+const myInstanceof2 = (left, right) => {
+  if (typeof left !== 'object' || left === null) {
+    return false;
+  }
+
+  let proto = Object.getPrototypeOf(left);
+  while (proto !== null) {
+    if (proto === right.prototype) return true;
+    proto = Object.getPrototypeOf(proto); // 沿着原型链继续向上寻找
+  }
+  return false;
+};
 
 let bool = myInstanceof({}, Object);
 
